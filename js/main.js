01b02da43abedd9ce64330c6617c4664d85c0996
antipenko <sweetglory.victory@gmail.com>
2016-02-03 04:27:20 +0200

    add responsive and js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
 ;(function(){
+	$(function(){
+		var body=$('body'),
+			menuToggle=$('.ba-menu-toggle');
 
-})();
+		menuToggle.click(function(event){
+			console.log(menuToggle);
+			event.preventDefault();
+			body.toggleClass('ba-menu-opened');
+			console.log('hw');
+		});
+	});
+})(jQuery);
+
+
